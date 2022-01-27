8a36d841fb2e7f796896b2145b38d9f5bc499bd5
kyosuke-bit <76424976+kyosuke-bit@users.noreply.github.com>
2022-01-27 16:06:29 +0900

    Postモデルの作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  before_action :set_target_post, only: %i[show edit update destroy]
+
+  def index
+    @posts = Post.page(params[:page])
+  end
+
+  def new
+    @post = Post.new(flash[:post])
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      flash[:notice] = "「#{post.title}」の質問を作成しました。"
+      redirect_to post
+    else
+      redirect_to new_post_path, flash: {
+        post: post,
+        error_messages: post.errors.full_messages
+      }
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to @post, flash: { notice: "#{@post.title}の質問が更新されました  " }
+  end
+
+  def destroy
+    @post.delete
+    redirect_to posts_path, flash: { notice: "#{@post.title}の質問が削除されました  " }
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def set_target_post
+    @post = Post.find(params[:id])
+  end
+end
