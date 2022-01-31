e60b1a645cab5d925903e65d7d90f367e2638ca6
kyosuke-bit <76424976+kyosuke-bit@users.noreply.github.com>
2022-01-31 17:16:15 +0900

    ヘッダーの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def header_link_item(name, path)
+    class_name = 'nav-link'
+    class_name << ' active' if current_page?(path)
+
+    content_tag :li, class: 'nav-item' do
+      link_to name, path, class: class_name
+    end
+  end
 end
