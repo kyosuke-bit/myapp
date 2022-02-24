4706cd3b8cbe92879222e1cdf4d621c0650c7daa
kyosuke-bit <76424976+kyosuke-bit@users.noreply.github.com>
2022-02-24 16:11:29 +0900

    ユーザー登録、ログインの挙動を修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'posts#index'
+  root 'home#index'
   # root 'home#index'
   resources :users, only: %i[new create] do
     member do
