2268a92a1df9a8b93144e8e49190fd4d935ea277
kyosuke-bit <76424976+kyosuke-bit@users.noreply.github.com>
2022-06-01 16:53:29 +0900

    不要なコードの削除

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -11,23 +11,14 @@ class RoomsController < ApplicationController
     @room = Room.find(params[:id])
     if Entry.where(room_id: @room.id, user_id: @current_user.id).present?
       @messages = @room.messages.all
-      # @message = Message.new
-      # @message = @room.messages.new
-      @message = Message.new(room_id: @room.id)
       @entries = @room.entries
-
       @user = (@entries.where(user_id: @current_user.id))[0]
       @another_user = (@entries.where.not(user_id: @current_user.id))[0]
+
+      @message = Message.new(room_id: @room.id)
     else
-      # redirect_to(fallback_location: link_to userpage_path(@user.name))
       redirect_back(fallback_location: root_path)
     end
 
   end
-
-  private
-
-  # def room_params
-  #   params.permit(user_ids: [])
-  # end
 end
